757e8d4539e0672cc70a412aa9852b6839f8ca32
sayali <sayalinimje123.com>
2020-01-04 16:44:20 +0530

    add file

---
+++ 1pro.py
@@ -0,0 +1,19 @@
+print("hello")
+l1=['raj','amit','aditi','divya']
+
+for i in l1:
+      print(i)
+    
+l2={1:'raj',2:'divya',3:'sagar'}
+for i in l2:
+      print(i)
+
+a=12
+b=70
+def sum1():
+      return a+b
+print('Total is :',sum1())
+
+
+
+
